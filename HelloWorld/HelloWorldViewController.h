3578c4eaafad32e345c2a3edb37a5a710e2cc3ab
mac <mac@macs-Mac.local>
2011-08-02 01:15:48 +0200

    Initial Commit

---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by mac on 8/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController {
+    
+}
+
+@end
---
+++ HelloWorldTests/HelloWorldTests.h
@@ -0,0 +1,17 @@
+//
+//  HelloWorldTests.h
+//  HelloWorldTests
+//
+//  Created by mac on 8/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface HelloWorldTests : SenTestCase {
+@private
+    
+}
+
+@end
